db468b6321ad9bd5fc1aede12b7e0592aca4c0a7
VolodiaHunkalo <graperz123@gmail.com>
2022-06-16 15:12:18 +0300

    auth complete

--- lib/screens/auth/LoginPage.dart
+++ lib/screens/auth/LoginPage.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mentority/models/user.dart';
 import 'package:mentority/screens/auth/widgets/LoginBottomNavyBar.dart';
-import 'package:mentority/screens/auth/widgets/LoginPageBody.dart';
+import 'package:mentority/screens/mentors/mentors_screen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,10 +14,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void checkValues () {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    if(email == '' || password == '') {
+      print('Please, fill all the fields!');
+    }
+    else {
+      logIn(email, password);
+    }
+  }
+
+  void logIn(String email, String password) async{
+    UserCredential? credential;
+
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch(ex) {
+      print(ex.message.toString());
+    }
+
+    if (credential != null) {
+      String uid = credential.user!.uid;
+
+      DocumentSnapshot userData = await FirebaseFirestore.instance
+          .collection('users').doc(uid).get();
+
+      UserModel userModel = UserModel
+          .fromMap(userData.data() as Map<String, dynamic>);
+
+      print('LogIn Successful!');
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoginPageBody(),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Text('Mentority', style:
+                  TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 50),
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        labelText: 'Email Adress'
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: 'Password'
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  CupertinoButton(
+                    onPressed: () {
+                      checkValues();
+                    },
+                    color: Colors.blue,
+                    child: Text('Log in'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: LoginBottomNavyBar(),
     );
   }
--- lib/screens/auth/SignUpPage.dart
+++ lib/screens/auth/SignUpPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mentority/screens/auth/widgets/SignUpBody.dart';
+import 'package:mentority/models/user.dart';
 import 'package:mentority/screens/auth/widgets/SignUpBottomNavyBar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -11,10 +13,101 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController cPasswordController = TextEditingController();
+
+  void checkValues () {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+    String cPassword = cPasswordController.text.trim();
+
+    if(email == '' || password == '' || cPassword == ''){
+      print('Please, fill all the fields!');
+    }
+    else if(password != cPassword){
+      print('Passwords do not match');
+    }
+    else {
+      signUp(email, password);
+    }
+  }
+
+  void signUp(String email, String password) async {
+    UserCredential? credential;
+
+    try{
+      credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+    } on FirebaseAuthException catch(ex) {
+      print(ex.code.toString());
+    }
+    if(credential != 0) {
+      String uid = credential!.user!.uid;
+      UserModel newUser = UserModel(
+        uid: uid,
+        email: email,
+        fullname: '',
+        profilepic: '',
+      );
+      await FirebaseFirestore.instance.collection('users').doc(uid)
+          .set(newUser.toMap()).then((value) {
+            print('New User Created!');
+      });
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: SignUpBody(),
+     body: SafeArea(
+       child: Container(
+         padding: EdgeInsets.symmetric(horizontal: 20),
+         child: Center(
+           child: SingleChildScrollView(
+             child: Column(
+               children: <Widget>[
+                 Text('Mentority', style:
+                 TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                 SizedBox(height: 50),
+                 TextField(
+                   controller: emailController,
+                   decoration: InputDecoration(
+                       labelText: 'Email Adress'
+                   ),
+                 ),
+                 SizedBox(height: 20),
+                 TextField(
+                   controller: passwordController,
+                   obscureText: true,
+                   decoration: InputDecoration(
+                       labelText: 'Password'
+                   ),
+                 ),
+                 SizedBox(height: 20),
+                 TextField(
+                   controller: cPasswordController,
+                   obscureText: true,
+                   decoration: InputDecoration(
+                       labelText: 'Confirm Password'
+                   ),
+                 ),
+                 SizedBox(height: 40),
+                 CupertinoButton(
+                   onPressed: () {
+                     checkValues();
+                   },
+                   color: Colors.blue,
+                   child: Text('Sign Up'),
+                 )
+               ],
+             ),
+           ),
+         ),
+       ),
+     ),
       bottomNavigationBar: SignUpBottomNavyBar(),
     );
   }
